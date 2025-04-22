a174efc810b2b6052b50ef3ce2a2ec4297c567b4
MI <pixillium.official@gmail.com>
2025-04-22 22:03:34 +0600

    Update ESLint configuration, enhance Next.js image settings, and increment version to 1.1.1. Refactor ServiceLogo and AuthenticatorCard components for improved readability and maintainability.

--- src/components/base/authenticator-card.tsx
+++ src/components/base/authenticator-card.tsx
@@ -27,7 +27,7 @@ import {
 import { Progress } from "@/components/ui/progress";
 import { useAppContext } from "@/lib/context";
 import EditAuthenticatorDialog from "./edit-authenticator-dialog";
-import ServiceLogo from "./service-logo"
+import ServiceLogo from "./service-logo";
 import { toast } from "sonner";
 import {
   AlertDialog,
@@ -115,11 +115,12 @@ export default function AuthenticatorCard({
         </div>
         <CardHeader>
           <div className="flex justify-between items-start">
-          	 <ServiceLogo name={authenticator.name} />
+            <ServiceLogo name={authenticator.name} />
             <div>
               <h3 className="font-semibold text-lg">{authenticator.name}</h3>
-              <p className="text-sm text-muted-foreground">{authenticator.email ? authenticator.email : "No email"}</p>
-              )}
+              <p className="text-sm text-muted-foreground">
+                {authenticator.email ? authenticator.email : "No email"}
+              </p>
             </div>
             <div className="flex items-center gap-2">
               {group && (
